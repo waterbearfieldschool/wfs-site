949e5461f373d212d5c96bb07e4766e680b689e4
Don Blair <donblair@gmail.com>
2025-10-15 16:43:12 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git add *
 git commit -m 'update'
 git push --no-verify
 rm -rf ../waterbearfieldschool.github.io/*
-cp -r dist/* ../waterbearfieldschool.github.io/
+cp -r _site/* ../waterbearfieldschool.github.io/
 cp CNAME ../waterbearfieldschool.github.io/
 cd ../waterbearfieldschool.github.io/
 git add *	
