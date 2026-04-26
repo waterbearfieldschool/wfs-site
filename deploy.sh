e52a9fb72dfa6b6032d4fc4fc73d1c1b17ff71b8
iris-refresher <dwblair+iris@gmail.com>
2026-04-26 14:27:47 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-git pull
 git add *
 git commit -m 'update'
 git push --no-verify
-cd ../waterbearfieldschool.github.io/
-git pull
-rm -rf *
-cd ../wfs-site/
+rm -rf ../waterbearfieldschool.github.io/*
 cp -r _site/* ../waterbearfieldschool.github.io/
 cp CNAME ../waterbearfieldschool.github.io/
 cd ../waterbearfieldschool.github.io/
 git add *	
 git commit -m 'update'
-git push
+git push -f
